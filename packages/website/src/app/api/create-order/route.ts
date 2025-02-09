a22c6639d36762aff7988e7a9503c62b505d07b2
Yash Goyal <yashgo0018@gmail.com>
2025-02-09 12:23:11 +0000

    added routes for creating orders and feedbacks

---
+++ packages/website/src/app/api/create-order/route.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+const createFeedbackValidator = z.object({
+  feedback: z.string(),
+  orderId: z.string(),
+});
+
+export const POST = async (req: NextRequest) => {
+  const authToken =
+    req.headers.get("x-api-key") || req.headers.get("X-API-KEY");
+
+  const rawData = await req.json();
+  console.log(rawData);
+  const result = createFeedbackValidator.safeParse(rawData);
+
+  if (!result.success) {
+    return NextResponse.json({ errors: result.error.issues }, { status: 400 });
+  }
+
+  // TODO: create order on blockchain
+
+  // TODO: store order in bnb greenfield solution
+
+  return NextResponse.json({ msg: "created order" });
+};
